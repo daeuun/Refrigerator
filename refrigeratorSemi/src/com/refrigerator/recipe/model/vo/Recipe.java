529395c43369dc99509f62863b99d3368efd7777
HeeRak <75523290+dexter1077@users.noreply.github.com>
2021-05-31 21:15:28 +0900

    Create Recipe.java

---
+++ refrigeratorSemi/src/com/refrigerator/recipe/model/vo/Recipe.java
@@ -0,0 +1,179 @@
+package com.refrigerator.recipe.model.vo;
+
+/**
+ * @author HeeRak
+ */
+public class Recipe {
+	
+	private int recipeNo;
+	private int userNo;
+	private int recipeTitle;
+	private String recipeIntro;
+	private int severalServings; 
+	private int cookingTime;
+	private int avrgStarPoint; 
+	private int count;
+	private int likeCount;
+	private int scrapCount;
+	private String recipeEnrollDate;
+	private String status;
+	private String mainImg;
+	private String ingreImg;
+	
+	public Recipe() {}
+	
+	public Recipe(int userNo, int recipeTitle, String recipeIntro, int severalServings, int cookingTime, String mainImg,
+			String ingreImg) {
+		super();
+		this.userNo = userNo;
+		this.recipeTitle = recipeTitle;
+		this.recipeIntro = recipeIntro;
+		this.severalServings = severalServings;
+		this.cookingTime = cookingTime;
+		this.mainImg = mainImg;
+		this.ingreImg = ingreImg;
+	}
+
+	public Recipe(int recipeNo, int userNo, int recipeTitle, String recipeIntro, int severalServings, int cookingTime,
+			int avrgStarPoint, int count, int likeCount, int scrapCount, String recipeEnrollDate, String status,
+			String mainImg, String ingreImg) {
+		super();
+		this.recipeNo = recipeNo;
+		this.userNo = userNo;
+		this.recipeTitle = recipeTitle;
+		this.recipeIntro = recipeIntro;
+		this.severalServings = severalServings;
+		this.cookingTime = cookingTime;
+		this.avrgStarPoint = avrgStarPoint;
+		this.count = count;
+		this.likeCount = likeCount;
+		this.scrapCount = scrapCount;
+		this.recipeEnrollDate = recipeEnrollDate;
+		this.status = status;
+		this.mainImg = mainImg;
+		this.ingreImg = ingreImg;
+	}
+
+	public int getRecipeNo() {
+		return recipeNo;
+	}
+
+	public void setRecipeNo(int recipeNo) {
+		this.recipeNo = recipeNo;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public int getRecipeTitle() {
+		return recipeTitle;
+	}
+
+	public void setRecipeTitle(int recipeTitle) {
+		this.recipeTitle = recipeTitle;
+	}
+
+	public String getRecipeIntro() {
+		return recipeIntro;
+	}
+
+	public void setRecipeIntro(String recipeIntro) {
+		this.recipeIntro = recipeIntro;
+	}
+
+	public int getSeveralServings() {
+		return severalServings;
+	}
+
+	public void setSeveralServings(int severalServings) {
+		this.severalServings = severalServings;
+	}
+
+	public int getCookingTime() {
+		return cookingTime;
+	}
+
+	public void setCookingTime(int cookingTime) {
+		this.cookingTime = cookingTime;
+	}
+
+	public int getAvrgStarPoint() {
+		return avrgStarPoint;
+	}
+
+	public void setAvrgStarPoint(int avrgStarPoint) {
+		this.avrgStarPoint = avrgStarPoint;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getLikeCount() {
+		return likeCount;
+	}
+
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+
+	public int getScrapCount() {
+		return scrapCount;
+	}
+
+	public void setScrapCount(int scrapCount) {
+		this.scrapCount = scrapCount;
+	}
+
+	public String getRecipeEnrollDate() {
+		return recipeEnrollDate;
+	}
+
+	public void setRecipeEnrollDate(String recipeEnrollDate) {
+		this.recipeEnrollDate = recipeEnrollDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMainImg() {
+		return mainImg;
+	}
+
+	public void setMainImg(String mainImg) {
+		this.mainImg = mainImg;
+	}
+
+	public String getIngreImg() {
+		return ingreImg;
+	}
+
+	public void setIngreImg(String ingreImg) {
+		this.ingreImg = ingreImg;
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe [recipeNo=" + recipeNo + ", userNo=" + userNo + ", recipeTitle=" + recipeTitle + ", recipeIntro="
+				+ recipeIntro + ", severalServings=" + severalServings + ", cookingTime=" + cookingTime
+				+ ", avrgStarPoint=" + avrgStarPoint + ", count=" + count + ", likeCount=" + likeCount + ", scrapCount="
+				+ scrapCount + ", recipeEnrollDate=" + recipeEnrollDate + ", status=" + status + ", mainImg=" + mainImg
+				+ ", ingreImg=" + ingreImg + "]";
+	}
+	
+
+}
