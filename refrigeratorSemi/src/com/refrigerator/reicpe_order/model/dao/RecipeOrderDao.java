8048ec8d445b4c2678f0e2230ccdba0fa5701621
HeeRak <75523290+dexter1077@users.noreply.github.com>
2021-06-03 18:18:31 +0900

    recipeorder 일부 수정 및 삭제

--- refrigeratorSemi/src/com/refrigerator/reicpe_order/model/dao/RecipeOrderDao.java
+++ refrigeratorSemi/src/com/refrigerator/reicpe_order/model/dao/RecipeOrderDao.java
@@ -21,53 +21,12 @@ public class RecipeOrderDao {
 	public RecipeOrderDao() {
 		
 		try {
-			prop.loadFromXML(new FileInputStream(RecipeOrderDao.class.getResource("/sql/recipe_order-mapper.xml").getPath()));
+			prop.loadFromXML(new FileInputStream(RecipeOrderDao.class.getResource("/sql/recipe_order/recipe_order-mapper.xml").getPath()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 	}
 
-	/**
-	 * 레시피등록페이지에 입력된 요리순서 insert 요청처리
-	 * @author HeeRak
-	 */
-	public int insertRecipeOrderList(Connection conn, ArrayList<RecipeOrder> ordList) {
-		int result = 1;
-		PreparedStatement pstmt = null;
-		String sql = prop.getProperty("insertRecipeOrderList");
-		String sql2 = prop.getProperty("insertNoImgRecipeOrderList");
-		
-		try {
-			
-			for(int i=0; i<ordList.size(); i++) {
-				
-				if(ordList.get(i).getRecipeImg()!= null) {// 이미지 있는 경우
-					
-					pstmt = conn.prepareStatement(sql);
-					pstmt.setInt(1, ordList.get(i).getRecipeOrder());
-					pstmt.setString(2, ordList.get(i).getRecipeExpln());
-					pstmt.setString(3, ordList.get(i).getRecipeImg());
-					
-					result = result * pstmt.executeUpdate();
-					
-				}else {// 이미지 없는경우
-					
-					pstmt = conn.prepareStatement(sql2);
-					pstmt.setInt(1, ordList.get(i).getRecipeOrder());
-					pstmt.setString(2, ordList.get(i).getRecipeExpln());
-					
-					result = result * pstmt.executeUpdate();
-				}
-				
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close(pstmt);
-		}
-		
-		return result;
-	}
-	
+
 }
--- refrigeratorSemi/src/com/refrigerator/reicpe_order/model/service/RecipeOrderService.java
+++ refrigeratorSemi/src/com/refrigerator/reicpe_order/model/service/RecipeOrderService.java
@@ -12,22 +12,5 @@ import com.refrigerator.reicpe_order.model.vo.RecipeOrder;
  */
 public class RecipeOrderService {
 
-	/**
-	 * 레시피등록페이지에 입력된 요리순서 insert 요청처리
-	 * @author HeeRak
-	 */
-	public int insertRecipeOrderList(ArrayList<RecipeOrder> ordList) {
-		Connection conn = getConnection();
-		int result = new RecipeOrderDao().insertRecipeOrderList(conn, ordList);
-		if(result > 0) {
-			commit(conn);
-		}else {
-			rollback(conn);
-		}
-		close(conn);
-		return result;
-	}
-	
-	
 	
 }
