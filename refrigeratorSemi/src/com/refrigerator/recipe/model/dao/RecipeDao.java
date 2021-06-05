151d8f5037eb3f0c8d2cb90487a12d460c72234b
LEEYEJI501 <yeaji0024@gmail.com>
2021-06-05 13:11:39 +0900

    RecipeDao 주석처리

--- refrigeratorSemi/src/com/refrigerator/recipe/model/dao/RecipeDao.java
+++ refrigeratorSemi/src/com/refrigerator/recipe/model/dao/RecipeDao.java
@@ -598,6 +598,7 @@ public class RecipeDao{
 	 * @author leeyeji
 	 * 특정 유저 레시피 목록 조회
 	 */
+	/*
 	public ArrayList<Recipe> selectUserRecipeList(Connection conn, PageInfo pi){
 		// select => ResultSet 여러행
 		ArrayList<Recipe> list = new ArrayList<>();
@@ -606,7 +607,7 @@ public class RecipeDao{
 		String sql = prop.getProperty("selectUserRecipeList");
 		
 		
-	}
+	}*/
 	
 	
 	
